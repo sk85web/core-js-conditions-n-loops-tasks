5170697e1e04ee0cfcf9d270c6514f36cdafd66d
Sergey <sk85post@gmail.com>
2024-01-19 22:15:32 +0300

    feat: complete isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -217,8 +217,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reverseStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reverseStr += str[i];
+  }
+  return str === reverseStr;
 }
 
 /**
